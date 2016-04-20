110ea695c4592e148d975f48cf3efaa7ce46bba8
Jason Song <wolfogre@outlook.com>
2016-04-21 01:06:42 +0800

    New tool: LR0_Analyser

---
+++ Exp3_AnalysingGrammar/LR0_Analyser.h
@@ -0,0 +1,202 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+class LR0_Analyser
+{
+public:
+
+
+	LR0_Analyser(std::vector<std::string> inputs)
+	{
+		this->inputs = inputs;
+		this->inputs.push_back(END_OF_INPUT);
+	}
+
+	void AddProduction(std::string left, std::string right1)
+	{
+		std::vector<std::string> right_list;
+		right_list.push_back(right1);
+		production_list.push_back(std::pair<std::string, std::vector<std::string>>(left, right_list));
+	}
+
+	void AddProduction(std::string left, std::string right1, std::string right2)
+	{
+		std::vector<std::string> right_list;
+		right_list.push_back(right1);
+		right_list.push_back(right2);
+		production_list.push_back(std::pair<std::string, std::vector<std::string>>(left, right_list));
+	}
+
+	void AddProduction(std::string left, std::string right1, std::string right2, std::string right3)
+	{
+		std::vector<std::string> right_list;
+		right_list.push_back(right1);
+		right_list.push_back(right2);
+		right_list.push_back(right3);
+		production_list.push_back(std::pair<std::string, std::vector<std::string>>(left, right_list));
+	}
+
+	void AddProduction(std::string left, std::string right1, std::string right2, std::string right3, std::string right4)
+	{
+		std::vector<std::string> right_list;
+		right_list.push_back(right1);
+		right_list.push_back(right2);
+		right_list.push_back(right3);
+		right_list.push_back(right4);
+		production_list.push_back(std::pair<std::string, std::vector<std::string>>(left, right_list));
+	}
+
+	void AddProduction(std::string left, std::string right1, std::string right2, std::string right3, std::string right4, std::string right5)
+	{
+		std::vector<std::string> right_list;
+		right_list.push_back(right1);
+		right_list.push_back(right2);
+		right_list.push_back(right3);
+		right_list.push_back(right4);
+		right_list.push_back(right5);
+		production_list.push_back(std::pair<std::string, std::vector<std::string>>(left, right_list));
+	}
+
+	void AddAction(int status, std::string input, char type, int value)
+	{
+		Action new_action;
+		new_action.status = status;
+		new_action.input = input;
+		new_action.type = type;
+		new_action.value = value;
+		action_list.push_back(new_action);
+	}
+
+	void AddGoto(int status, std::string input, int value)
+	{
+		Goto new_goto;
+		new_goto.status = status;
+		new_goto.input = input;
+		new_goto.value = value;
+		goto_list.push_back(new_goto);
+	}
+
+	int Analyse()
+	{
+		std::vector<int> status_stack;
+		std::vector<std::string> chars_stack;
+
+		status_stack.push_back(0);
+		size_t index = 0;
+
+		for (index = 0; index < inputs.size(); ++index)
+		{
+			Show(status_stack);
+			Show(chars_stack);
+
+			char type;
+			int value;
+			if (!getAction(status_stack.back(), inputs[index], type, value))
+				return index;
+
+			if (type == 's' || type == 'S')
+			{
+				status_stack.push_back(value);
+				chars_stack.push_back(inputs[index]);
+				continue;
+			}
+
+			if (type == 'r' || type == 'R')
+			{
+				--index;
+				std::pair<std::string, std::vector<std::string>> production = production_list[value - 1];
+				while (!production.second.empty())
+				{
+					production.second.pop_back();
+					chars_stack.pop_back();
+					status_stack.pop_back();
+				}
+				chars_stack.push_back(production.first);
+				int goto_value;
+				if(!getGoto(status_stack.back(), production.first, goto_value))
+					return index;
+				status_stack.push_back(goto_value);
+				continue;
+			}
+
+			if (type == 'a' || type == 'A')
+			{
+				return -1;
+			}
+
+		}
+		
+		return index;
+	}
+
+	static const std::string END_OF_INPUT;
+	static const std::string ALL_OF_INPUT;
+
+protected:
+	struct Action
+	{
+		int status;
+		std::string input;
+		char type;
+		int value;
+	};
+
+	struct Goto
+	{
+		int status;
+		std::string input;
+		int value;
+	};
+
+	bool getAction(int status, std::string input, char &type, int &value)
+	{
+		for (Action action : action_list)
+		{
+			if (action.status == status && (action.input == input || action.input == ALL_OF_INPUT))
+			{
+				type = action.type;
+				value = action.value;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool getGoto(int status, std::string input, int &value)
+	{
+		for (Goto g : goto_list)
+		{
+			if (g.status == status && g.input == input)
+			{
+				value = g.value;
+				return true;
+			}
+		}
+		return false;
+	}
+	void Show(std::vector<int> status_stack)
+	{
+		for (auto it : status_stack)
+			std::cout << it << " ";
+		std::cout << std::endl;
+	}
+	void Show(std::vector<std::string> chars_stack)
+	{
+		std::cout << "#" << " ";
+		for (auto it : chars_stack)
+			std::cout << it << " ";
+		std::cout << std::endl;
+	}
+private:
+	
+	std::vector<std::string> inputs;
+	std::vector<Action> action_list;
+	std::vector<Goto> goto_list;
+	std::vector<std::pair<std::string, std::vector<std::string>>> production_list;
+};
+
+const std::string LR0_Analyser::END_OF_INPUT = "end_of_input_i_love_vivia";
+const std::string LR0_Analyser::ALL_OF_INPUT = "all_of_input_i_love_vivia";
--- Exp3_AnalysingGrammar/Main.cpp
+++ Exp3_AnalysingGrammar/Main.cpp
@@ -1,4 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "LR0_Analyser.h"
+
+
+using namespace std;
+
 int main()
 {
+	vector<string> inputs;
+	inputs.push_back("a");
+	inputs.push_back("b");
+	inputs.push_back("b");
+	inputs.push_back("c");
+	inputs.push_back("d");
+	inputs.push_back("e");
+
+	LR0_Analyser la(inputs);
+
+	la.AddProduction("S", "a", "A", "c", "B", "e");
+	la.AddProduction("A", "b");
+	la.AddProduction("A", "A", "b");
+	la.AddProduction("B", "d");
+
+	la.AddAction(0, "a", 'S', 2);
+	la.AddAction(1, LR0_Analyser::END_OF_INPUT, 'A', 0);
+	la.AddAction(2, "b", 'S', 4);
+	la.AddAction(3, "c", 'S', 5);
+	la.AddAction(3, "b", 'S', 6);
+	la.AddAction(4, LR0_Analyser::ALL_OF_INPUT, 'r', 2);
+	la.AddAction(5, "d", 'S', 8);
+	la.AddAction(6, LR0_Analyser::ALL_OF_INPUT, 'r', 3);
+	la.AddAction(7, "e", 'S', 9);
+	la.AddAction(8, LR0_Analyser::ALL_OF_INPUT, 'r', 4);
+	la.AddAction(9, LR0_Analyser::ALL_OF_INPUT, 'r', 1);
+
+	la.AddGoto(0, "S", 1);
+	la.AddGoto(2, "A", 3);
+	la.AddGoto(5, "B", 7);
+
+	cout << la.Analyse() << endl;
+
 	return 0;
 }
